d245f8c610911287b2df895d95507ee18d69e2ea
Clint Robison <admin@clintrobison.com>
2010-12-08 16:01:08 -0500

    lots of updates style and image detail page

--- www/tulsa/2.0/getInmate.php
+++ www/tulsa/2.0/getInmate.php
@@ -4,9 +4,14 @@ if($_GET['id']) {
     if (!$con) die('Could not connect to database: ' . mysql_error());
     mysql_select_db("db336192140", $con);
 
+    $race_array = array("W"=>"White", "B"=>"Black", "H"=>"Hispanic", "I"=>"Pacific Islander", "O"=>"Other");
+
     $query = "select * from records where personId=" . $_GET['id'];
     $result = mysql_query($query);
     $row = mysql_fetch_object($result);
+    $row->race = $race_array[$row->race];
+    $row->gender = $row->gender = "M" ? "Male" : "Female";
+
     echo json_encode($row);
     mysql_close();
 }
